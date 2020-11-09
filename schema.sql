61c44588f8cba0975c7914f0717bc71bae4c3bfd
yyu52 <yyu52@fordham.edu>
2020-11-09 20:39:47 +1300

    update

--- schema.sql
+++ schema.sql
@@ -36,8 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-	emp_no int NOT NULL,
-	title INT NOT NULL, 
+	emp_no INT NOT NULL,
+	title VARCHAR(20) NOT NULL, 
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
